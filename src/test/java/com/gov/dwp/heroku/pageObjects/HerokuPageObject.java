297a12f5849f1afd401dcb615ffbd768de22b15a
kgnana741 <kevin_gnanamuthu@comcast.com>
2020-07-20 23:23:23 -0400

    Removed unwanted model class.

--- src/test/java/com/gov/dwp/heroku/pageObjects/HerokuPageObject.java
+++ src/test/java/com/gov/dwp/heroku/pageObjects/HerokuPageObject.java
@@ -7,5 +7,4 @@ public class HerokuPageObject {
     public static By jsonLink = By.xpath("//*[@id=\"swagger-ui\"]/div/div/div[1]/section/div/hgroup/a");
     public static By endPointLink = By.xpath("//*[@id=\"operations-tag-default\"]/small/div/p");
     public static By endPointList = By.xpath("//*[@id=\"swagger-ui\"]/div/div/div[2]/section/div/div");
-
 }
